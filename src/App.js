8e0b5df6637dfde638b335ce1c256e763193615d
Gujansko <mateusz.klos150401@gmail.com>
2024-11-22 19:53:00 +0100

    Add email changed route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ResetPassword from "./ResetPassword/ResetPassword";
+import EmailChanged from "./EmailChanged/EmailChanged";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="/email-changed" element={<EmailChanged />} />
       </Routes>
     </Router>
   );
